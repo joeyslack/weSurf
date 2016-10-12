79241605946447321f41ff469b45b256be12c117
Joseph Slack <josephdslack@gmail.com>
2016-10-12 11:47:31 -0700

    object model

---
+++ object_model.js
@@ -0,0 +1,40 @@
+"surfs": {
+	"-KKoGRheRQqA-15ij0oS": {
+		"title": "My cool stream",
+		"created_at": "2016-08-01 12:01:12",
+		"tags": ["sports", "politics", "news"],
+		"user": "testUser",
+		"likes": 99,
+		"views": 3012,
+		"viewing": 12,
+		"viewers": ["xxx", "abc", "123", "def"],
+		"live": true,
+		"pages": {
+			"page1": {
+				"title": "Page Title Here",
+				"url": "http://www.hotmail.com.com"
+			},
+			"page2": {
+				"title": "Another page title",
+				"url": "http://www.google.com"
+			}
+		},
+		"offset": "100, 0"
+	}
+}
+
+"users": {
+	
+}
+
+"tags": {
+	
+}
+
+"like": {
+	
+}
+
+"view" {
+	
+}
